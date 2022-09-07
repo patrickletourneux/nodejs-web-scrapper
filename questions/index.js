2fc8b65453a79ae06bbe71cd57a9630997dd5f60
patrick letourneux <patrick.letourneux20@gmail.com>
2022-09-07 16:11:47 +0200

    eslint added

--- questions/index.js
+++ questions/index.js
@@ -1,26 +1,26 @@
 const questions = [
-    {
-      type: 'text',
-      name: 'url',
-      initial: 'url',
-      message: 'give a page to scrap'
-    },
-    {
-      type: 'text',
-      name: 'fileName',
-      initial: 'fileName',
-      message: 'give a filename '
-    },
-    {
-      type: 'select',
-      name: 'value',
-      message: 'make a choice',
-      choices: [
-        { title: 'screenshot', description: 'screenshot png file', value: 'screenshot' },
-        { title: 'wikiSummary', description: 'wiki summary page txt file', value: 'wikiSummary' },
-      ],
-      initial: 0
-    }
-  ];
+  {
+    type: 'text',
+    name: 'url',
+    initial: 'url',
+    message: 'give a page to scrap',
+  },
+  {
+    type: 'text',
+    name: 'fileName',
+    initial: 'fileName',
+    message: 'give a filename ',
+  },
+  {
+    type: 'select',
+    name: 'value',
+    message: 'make a choice',
+    choices: [
+      { title: 'screenshot', description: 'screenshot png file', value: 'screenshot' },
+      { title: 'wikiSummary', description: 'wiki summary page txt file', value: 'wikiSummary' },
+    ],
+    initial: 0,
+  },
+];
 
 module.exports = questions;
